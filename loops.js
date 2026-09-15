472b02aab58a3488241082cbf212e298629b2b3d
Asif Forizy <asifforizy@gmail.com>
2026-09-15 20:05:07 +0600

    loops

---
+++ loops.js
@@ -0,0 +1,38 @@
+// for loop 
+
+for (let i=0; i < 10; i++) {
+    console.log(i);
+}
+
+
+
+//  while loop
+
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+//  do while loop
+
+let j = 0;
+do {
+    console.log(j);
+    j++;
+}while (j < 10);
+
+
+
+//  sum of n natural number
+let n = 10 
+
+let sum = 0
+
+for (let i = 1; i <= n; i++) {
+    sum += i;
+}
+console.log(sum);
+
+
+
